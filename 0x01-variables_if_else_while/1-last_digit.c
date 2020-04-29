5916216156294b46bb2b0565e4c1e83540c21ae8
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-04-29 20:50:18 +0000

    The output of the second printf was corrected

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	else if (((n % 10) < 6) && (n % 10))
 	{
 		printf("Last digit of %d is %d ", n, n % 10);
-		printf("and is greater than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	}
 	else
 	{
