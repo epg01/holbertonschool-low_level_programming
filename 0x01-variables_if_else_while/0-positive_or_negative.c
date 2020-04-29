d14682b859af39a587bbb02d4334310c067984be
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-04-29 19:26:41 +0000

    function that prints a random number and checks if it is positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Function that prints a random number and indicate whether it is
+ *        it is possitive, negative or zero
+ * Return: Successful exit
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is possitive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
