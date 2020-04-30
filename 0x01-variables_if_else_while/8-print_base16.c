51a898b10a8a640c5977a472636d1921fc371fd6
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-04-30 03:46:56 +0000

    We organize the source file regarding Betty style requeriments

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ * main - prints all the numbers of base 16 in lowercase, followed
+ *        by a new line
  *
  * Return: exit successful.
  */
@@ -11,7 +12,7 @@ int main(void)
 	unsigned char NumberHexadecimal;
 
 	NumberHexadecimal = '0';
-	while(NumberHexadecimal != ('\n' + 1))
+	while (NumberHexadecimal != ('\n' + 1))
 	{
 		if (NumberHexadecimal == ('9' + 1))
 			NumberHexadecimal = 'a';
