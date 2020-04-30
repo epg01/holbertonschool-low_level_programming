84045640bf62bcae383c2036ee47682551c77b3f
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-04-30 15:47:28 +0000

    program that prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - program that prints the numbers from 00 to 99.
+ * Return: exit successful.
+ */
+int main(void)
+{
+	unsigned int Accountant1, Accountant2, Accountant3, StoredCharacter;
+
+	Accountant1 = '0', Accountant2 = '0';
+	Accountant3 = 0, StoredCharacter = '\0';
+
+	while (StoredCharacter != ('\n' + 1))
+	{
+		putchar(StoredCharacter);
+		if (Accountant2 > '9')
+		{
+			Accountant2 = '0';
+			Accountant1++;
+		}
+
+		if (StoredCharacter >= '0' && StoredCharacter <= '9')
+			Accountant3++;
+
+		if (Accountant3 == 1)
+		{
+			StoredCharacter = Accountant2;
+			Accountant2++;
+		}
+		if (Accountant3 == 2)
+		{
+			StoredCharacter = ',';
+			Accountant3 = 0;
+		}
+		else if (StoredCharacter == ',')
+			StoredCharacter = '_';
+		else if (StoredCharacter == '_')
+			StoredCharacter = Accountant1;
+
+		/* Inicialización */
+
+		if (StoredCharacter == '\n')
+			StoredCharacter++;
+		else if (Accountant1 == ('0' + 1))
+			StoredCharacter = '\n';
+	}
+	return (0);
+}
