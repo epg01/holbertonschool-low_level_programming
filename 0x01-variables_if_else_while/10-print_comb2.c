95c5855a4d8bfbf86783fed5068f7d7302e599c4
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-04-30 17:02:12 +0000

    organizing the file according to the requirements of betty style and documenting the code

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,35 +7,36 @@ int main(void)
 {
 	unsigned int Accountant1, Accountant2, StoredCharacter, Accountant3;
 
-	Accountant1 = '0';
-	Accountant2 = '0';
+	Accountant1 = '0';    /* this variable controls the first character */
+	Accountant2 = '0';    /* this variable controls the second character */
+/* Indicates whether or not StoredCharacter is in a number */
 	Accountant3 = 0;
 	StoredCharacter = '0';
 
-	while(StoredCharacter != ('\n' + 1))
+	while (StoredCharacter != ('\n' + 1))
 	{
 		putchar(StoredCharacter);
 
 		if (Accountant2 > '9')
-		{
-			Accountant2 = '0';
-			Accountant1++;
-		}
+			Accountant2 = '0', Accountant1++;
 
 		if (StoredCharacter >= '0' && StoredCharacter <= '9')
 			Accountant3++;
 
 		if (Accountant3 == 1)
-		{
-			StoredCharacter = Accountant2;
-			Accountant2++;
-		}
+			StoredCharacter = Accountant2, Accountant2++;
+
 		if (Accountant3 == 2)
 			StoredCharacter = ',', Accountant3 = 0;
-		else if(StoredCharacter == ',')
+		else if (StoredCharacter == ',')
 			StoredCharacter = ' ';
 		else if (StoredCharacter == ' ')
 			StoredCharacter = Accountant1;
+
+/*
+ * this nested if controls the while condition, when Accountant reaches 10,
+ * is where we have to print the line break
+ */
 
 		if (StoredCharacter == '\n')
 			StoredCharacter++;
