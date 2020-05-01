05406ad73e3873ac0e7d1dfd8f430010a4c677a4
epg01 <60383826+epg01@users.noreply.github.com>
2020-05-01 17:38:33 -0500

    was returned to the penultimate commit

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,43 +5,43 @@
  */
 int main(void)
 {
-	unsigned int FirstDigit, SecondDigit, StoredCharacter, Accountant;
+        unsigned int Accountant1, Accountant2, StoredCharacter, Accountant3;
 
-	FirstDigit = '0';
-	SecondDigit = '0';
+        Accountant1 = '0';    /* this variable controls the first character */
+        Accountant2 = '0';    /* this variable controls the second character */
 /* Indicates whether or not StoredCharacter is in a number */
-	Accountant = 0;   /* it is used to know if StoredCharacter is in a digit or not*/
-	StoredCharacter = '0';
+        Accountant3 = 0;
+        StoredCharacter = '0';
 
-	while (StoredCharacter != ('\n' + 1))
-	{
-		putchar(StoredCharacter);
+        while (StoredCharacter != ('\n' + 1))
+        {
+                putchar(StoredCharacter);
 
-		if SecondDigit > '9')
-			SecondDigit = '0', FirstDigit++;
+                if (Accountant2 > '9')
+                        Accountant2 = '0', Accountant1++;
 
-		if (StoredCharacter >= '0' && StoredCharacter <= '9')
-			Accountant++;
+                if (StoredCharacter >= '0' && StoredCharacter <= '9')
+                        Accountant3++;
 
-		if (Accountant3 == 1)
-			StoredCharacter = SecondDigit, SecondDigit++;
+                if (Accountant3 == 1)
+                        StoredCharacter = Accountant2, Accountant2++;
 
-		if (Accountant3 == 2)
-			StoredCharacter = ',', Accountant = 0;
-		else if (StoredCharacter == ',')
-			StoredCharacter = ' ';
-		else if (StoredCharacter == ' ')
-			StoredCharacter = FirstDigit;
+                if (Accountant3 == 2)
+                        StoredCharacter = ',', Accountant3 = 0;
+                else if (StoredCharacter == ',')
+                        StoredCharacter = ' ';
+                else if (StoredCharacter == ' ')
+                        StoredCharacter = Accountant1;
 
 /*
  * this nested if controls the while condition, when Accountant reaches 10,
  * is where we have to print the line break
  */
 
-		if (StoredCharacter == '\n')
-			StoredCharacter++;
-		else if (FirstDigit == ('9' + 1))
-			StoredCharacter = '\n';
-	}
-	return (0);
+                if (StoredCharacter == '\n')
+                        StoredCharacter++;
+                else if (Accountant1 == ('9' + 1))
+                        StoredCharacter = '\n';
+        }
+        return (0);
 }
