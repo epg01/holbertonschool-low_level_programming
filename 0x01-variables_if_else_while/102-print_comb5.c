272ebbaff0bf36fdc9e06b79b86c61fc272467eb
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-01 16:27:21 +0000

    organized with betty's requests

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
+/**
+ *main - program that prints all possible combinations of two two-digit numbers
+ * Return: exit successfull.
+ */
 int main(void)
 {
-	unsigned int FirstDigit, SecondDigit, FourthDigit, StoredCharacter, ThirdDigit;
-	unsigned int Accountant;
+	unsigned int FirstDigit, SecondDigit, FourthDigit, StoredCharacter;
+	unsigned int Accountant, ThirdDigit;
 
-	FirstDigit = '0';
-	SecondDigit = '0';
-	ThirdDigit = '0';
-	FourthDigit = '1';
-	StoredCharacter = FirstDigit;
-
-	Accountant = 0;
+	FirstDigit = SecondDigit = ThirdDigit = '0';
+	FourthDigit = '1', StoredCharacter = FirstDigit, Accountant = 0;
 
 	while (StoredCharacter != ('\n' + 1))
 	{
 		putchar(StoredCharacter);
 
 		if (FourthDigit == ('9' + 1))
-		{
-			ThirdDigit++;
-			FourthDigit = '0';
-		}
-
+			ThirdDigit++, FourthDigit = '0';
 		if (ThirdDigit == ('9' + 1))
 			SecondDigit++, ThirdDigit = FirstDigit, FourthDigit = SecondDigit + 1;
 		if (SecondDigit == ('9' + 1))
-			FirstDigit++, SecondDigit = '0', ThirdDigit = FirstDigit, FourthDigit = SecondDigit + 1;
-
+		{
+			FirstDigit++, SecondDigit = '0';
+			ThirdDigit = FirstDigit, FourthDigit = SecondDigit + 1;
+		}
 		if (Accountant == 0)
 			StoredCharacter = SecondDigit, Accountant++;
 		else if (Accountant == 1)
@@ -41,12 +38,11 @@ int main(void)
 			StoredCharacter = ' ', Accountant++;
 		else if (Accountant == 6)
 			StoredCharacter = FirstDigit, Accountant = 0;
-
 		if (StoredCharacter == '\n')
 			StoredCharacter++;
-		else if ((FirstDigit == '9') && (FourthDigit == ('9' + 1)) && (Accountant == 5))
+		else if ((FirstDigit == '9') && (FourthDigit == ('9' + 1))
+			 && (Accountant == 5))
 			StoredCharacter = '\n';
-
 	}
 	return (0);
 }
