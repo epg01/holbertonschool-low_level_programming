57991aabc0e4a09c9687aad69aff897d85e44964
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-18 02:22:01 +0000

    added a function and fixed documentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,9 @@
  * Certain symbolic constants are defined
  */
 
-#define VALUEFORCHARACTERS    'a';
-#define INITIALIZERFORCOLUMNS  00;
-#define FORTHEROWS             0XA;
-
-/*
- * An external variable is defined
- */
-
-unsigned CharcharacterCounter;
+#define VALUEFORCHARACTERS    'a'
+#define INITIALIZERFORCOLUMNS  00
+#define FORTHEROWS             0XA
 
 /*
  * A function is declared or as the prototype function is sometimes called
@@ -20,10 +14,15 @@ unsigned CharcharacterCounter;
 
 unsigned char CalculateTheLength(void);
 
+/**
+ *   print_alphabet_x10 - function that prints 10 times the alphabet,
+ *                        in lowercase
+ */
+
 void print_alphabet_x10(void)
 {
 	unsigned char SolveCharacterAlpahabet;
-	unsigned char NumberOfLines, NumberOfColumns;
+	unsigned char NumberOfLines, NumberOfColumns, CharcharacterCounter;
 
 	CharcharacterCounter = CalculateTheLength();
 
@@ -34,22 +33,36 @@ void print_alphabet_x10(void)
 	{
 		while (NumberOfLines)
 		{
-			if ((NumberOfColumns < CharcharacterCounter) || (SolveCharacterAlpahabet == '\n'))
-				_putchar(SolveCharacterAlpahabet++), NumberOfColumns++;
+			if ((NumberOfColumns < CharcharacterCounter) ||
+			    (SolveCharacterAlpahabet == '\n'))
+			{
+				_putchar(SolveCharacterAlpahabet++);
+				NumberOfColumns++;
+			}
 			else if (NumberOfColumns == CharcharacterCounter)
 				SolveCharacterAlpahabet = '\n';
 			else
 			{
-				SolveCharacterAlpahabet = 'a', NumberOfColumns = 00;
-				NumberOfLines--;
+				SolveCharacterAlpahabet = 'a';
+				NumberOfColumns = 00, NumberOfLines--;
 			}
 		}
 	}
 	else
-		return ;
+		return;
 }
 
+/**
+ *  CalculateTheLength - calculates the length of the characters to be printed
+ *  @x: variable related to macro
+ */
+
+/*
+ *  Place where the macro used in the while condition was defined
+ */
+
 #define CONDITION_WHILE(x) (((x) >= ('a')) && ((x) <= ('z')))
+
 unsigned char CalculateTheLength(void)
 {
 	int Accountant;
