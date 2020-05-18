ba9014c947768ca2d22e5ad5a9f953dbddcb55f0
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-18 03:22:59 +0000

    It was organized with the demands of betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,21 @@
-/**
- *  _islower - Function that checks for lowercase character.
- *  @c: variable type int
- *  Return: Returns 1 if c is a lowercase character otherwise returns zero
- */
 /*
  * Two symbolic constants are defined
  */
 
 #define  LOWERCASE_LETTER    1
 #define  ANYTHING            0
+
 /*
  * A macro is defined
  */
 
 #define islower(Character) (((Character) >= ('a')) && ((Character) <= ('z')))
+
+/**
+ *  _islower - Function that checks for lowercase character.
+ *  @c: variable type int
+ *  Return: Returns 1 if c is a lowercase character otherwise returns zero
+ */
 
 int _islower(int c)
 {
