cb93494dd3a47202505d802a312c7a9e536d1261
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-19 04:27:48 +0000

    Added an enumeration statement

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#if !defined(HDR)
+#define HDR
+
 int _putchar(char c);
 void PrintCharacter(unsigned char ArrayOfCharacter[]);
 void print_alphabet(void);
@@ -11,3 +14,11 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+enum COLUMN
+{
+	COLUMN_ZERO, COLUMN_ONE, COLUMN_TWO, COLUMN_THREE,
+	COLUMN_FOUR, COLUMN_FIVE, NUMBER_OF_LINES = 1440
+};
+
+#endif
