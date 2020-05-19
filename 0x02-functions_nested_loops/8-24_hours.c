9c72c4dbec35d6a613327e8ae9b74b5f143b6b99
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-19 04:19:49 +0000

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,32 @@
 #include "holberton.h"
 
 /**
- *  jack_bauer -  function that prints every minute of the day
- *                of Jack Bauer, starting from 00:00 to 23:59.
+ *   jack_bauer - function that prints every minute of the day of
+ *                Jack Bauer, starting from 00:00 to 23:59.
+ *
  */
 
 void jack_bauer(void)
 {
 	unsigned char FirstDigit, SecondDigit, ThirdDigit, FourthDigit;
 	unsigned char SolveVariables, AccountantColumns;
-	unsigned int AccountantLine;
-
-	enum COLUMN
-	{
-		COLUMN_ZERO, COLUMN_ONE, COLUMN_TWO, COLUMN_THREE,
-		COLUMN_FOUR, COLUMN_FIVE, NUMBER_OF_LINES = 1440
-	};
+	unsigned int AccountantLine, State;
 
 	AccountantColumns = AccountantLine = 0;
 	FirstDigit = SecondDigit = ThirdDigit = FourthDigit = '0';
 	while (AccountantLine < NUMBER_OF_LINES)
 	{
-		if (AccountantColumns == COLUMN_ZERO)
+		if (SecondDigit == '9' + 1)
+			SecondDigit = '0', FirstDigit++;
+		else if (ThirdDigit == '6')
+			ThirdDigit = '0', SecondDigit++;
+		else if (FourthDigit == '9' + 1)
+			FourthDigit = '0', ThirdDigit++;
+		else if (AccountantColumns == COLUMN_ZERO)
+		{
 			SolveVariables =  FirstDigit, AccountantColumns++;
+			State = 1;
+		}
 		else if (AccountantColumns == COLUMN_ONE)
 			SolveVariables =  SecondDigit, AccountantColumns++;
 		else if (AccountantColumns == COLUMN_TWO)
@@ -36,14 +40,10 @@ void jack_bauer(void)
 		else
 		{
 			FourthDigit++, AccountantColumns = 0, AccountantLine++;
-			continue;
+			State = 0;
 		}
-		if (SecondDigit == '9' + 1)
-			SecondDigit = '0', FirstDigit++;
-		else if (ThirdDigit == '6')
-			ThirdDigit = '0', SecondDigit++;
-		else if (FourthDigit == '9' + 1)
-			FourthDigit = '0', ThirdDigit++;
-		_putchar(SolveVariables);
+
+		if (State)
+			_putchar(SolveVariables);
 	}
 }
