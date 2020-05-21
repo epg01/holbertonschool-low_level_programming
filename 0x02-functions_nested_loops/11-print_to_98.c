2b91c0350479a2144e9e67d72f107d8218133ab4
Emmanuel Palacio Gaviria <emmanuelpalacio1@gmail.com>
2020-05-21 22:33:33 +0000

    Some functions have been removed, others have been added and the problem has been organized

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,18 +5,17 @@
  * declared, I treat this as my main function.
  */
 
-#define NUMBER_BETWEEN_0_AND_9(Number) (((Number) >= (0)) && ((Number) < (98)))
+#define NUMBER_BETWEEN_0_AND_97(Number) (((Number) >= (0)) && ((Number) < 98))
 #define NUMBER_BEYOND_98(Number1) ((Number1) > (98))
-#define NUMBER_LESS_10(Number2) ((Number2) < (0))
-#define NEGATIVE_THE_1_AND_9(Numb3) (((Numb3) < (0)) && ((Numb3) < (-10)))
+#define NUMBER_LESS_0(Number2) ((Number2) < (0))
 
 /*
  * The functions to be used are declared.
  */
-void print_between_0_9(int Number);
+
+void print_between_0_97(int Number);
 void print_Beyond_98(int Number);
-void print_Less_10(int Number);
-void print_between_The_Negative_0_9(int Number);
+void print_Less_0(int Number);
 
 /**
  * print_to_98 - function that prints all natural numbers from n to 98,
@@ -26,98 +25,95 @@ void print_between_The_Negative_0_9(int Number);
 
 void print_to_98(int Number)
 {
-        unsigned char SolveCharacter, State0, State1;
+	unsigned char SolveCharacter, State0, State1;
 
-        SolveCharacter = 0, State0 = State1 = 1;
-        while (SolveCharacter != '\n')
-        {
-                if (NUMBER_BETWEEN_0_AND_9(Number))
-                        print_between_0_9(Number++);
-                else if (NUMBER_BEYOND_98(Number))
-                        print_Beyond_98(Number--);
-                else if (NEGATIVE_THE_1_AND_9(Number))
-                        print_between_The_Negative_0_9(Number++);
-                else if (NUMBER_LESS_10(Number))
-                        print_Less_10(Number++);
-                else if ((Number == 98) && (State0))
-                        SolveCharacter = Number / 10 + '0', State0 = 0;
-                else if (State1)
-                        SolveCharacter = Number % 10 + '0', State1 = 0;
-                else if (!State1)
-                        SolveCharacter = '\n';
+	SolveCharacter = 0, State0 = State1 = 1;
+	while (SolveCharacter != '\n')
+	{
+		if (NUMBER_LESS_0(Number))
+			print_Less_0(Number++);
+		else if (NUMBER_BETWEEN_0_AND_97(Number))
+			print_between_0_97(Number++);
+		else if (NUMBER_BEYOND_98(Number))
+			print_Beyond_98(Number--);
+		else if ((Number == 98) && (State0))
+			SolveCharacter = Number / 10 + '0', State0 = 0;
+		else if (State1)
+			SolveCharacter = Number % 10 + '0', State1 = 0;
+		else if (!State1)
+			SolveCharacter = '\n';
 
-                if (!State0)
-                        _putchar(SolveCharacter);
-        }
+		if (!State0)
+			_putchar(SolveCharacter);
+	}
 }
 
 /**
- * print_between_0_9 - Function that prints from 0 to 9.
+ * print_between_0_97 - Function that prints from 0 to 97.
  * @Number: variable type int.
  */
 
-void print_between_0_9(int Number)
+void print_between_0_97(int Number)
 {
-        if (Number < 10)
-        {
-                int SolveConditionOfWhile, SolveNewNumber;
-                unsigned char State0, State1, SolveCharacter;
+	if (Number < 10)
+	{
+		int SolveConditionOfWhile, SolveNewNumber;
+		unsigned char State0, State1, SolveCharacter;
 
-                SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber + 1;
-                State0 = State1 = 1;
+		SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber + 1;
+		State0 = State1 = 1;
 
-                while (Number < SolveConditionOfWhile)
-                {
-                        if (State0)
-                                SolveCharacter = Number % 10 + '0', State0 = 0;
-                        else if (State1)
-                                SolveCharacter = ',', State1 = 0;
-                        else if (SolveCharacter == ',')
-                                SolveCharacter = ' ';
-                        else if (SolveCharacter == ' ')
-                                Number++;
+		while (Number < SolveConditionOfWhile)
+		{
+			if (State0)
+				SolveCharacter = Number % 10 + '0', State0 = 0;
+			else if (State1)
+				SolveCharacter = ',', State1 = 0;
+			else if (SolveCharacter == ',')
+				SolveCharacter = ' ';
+			else if (SolveCharacter == ' ')
+				Number++;
 
-                        if (!(Number == SolveConditionOfWhile))
-                                _putchar(SolveCharacter);
-                }
-        }
-        else
-        {
-                unsigned char State0, State1, SolveCharacter;
-                int SolveConditionOfWhile, SolveNewNumber;
-                unsigned short int PowerOf10;
+			if (!(Number == SolveConditionOfWhile))
+				_putchar(SolveCharacter);
+		}
+	}
+	else
+	{
+		unsigned char State0, State1, SolveCharacter;
+		int SolveConditionOfWhile, SolveNewNumber;
+		unsigned short int PowerOf10;
 
-                State0 = State1 = 1;
-                SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber + 1;
-                PowerOf10 = 10;
+		State0 = State1 = 1;
+		SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber + 1;
+		PowerOf10 = 10;
 
-                while (Number < SolveConditionOfWhile)
-                {
-                        if ((State0) && (Number / PowerOf10))
-                        {
-                                PowerOf10 *= 10;
-                                continue;
-                        }
-                        else if (!(PowerOf10 == 1))
-                        {
-                                if (State0)
-                                        State0 = 0;
-                                PowerOf10 /= 10;
-                                SolveCharacter = SolveNewNumber / PowerOf10 + '0';
-                                SolveNewNumber %=  PowerOf10;
-                        }
-                        else if (State1)
-                                SolveCharacter = ',', State1 = 0;
-                        else if (SolveCharacter == ',')
-                                SolveCharacter = ' ';
-                        else if (SolveCharacter == ' ')
-                                Number++;
+		while (Number < SolveConditionOfWhile)
+		{
+			if ((State0) && (Number / PowerOf10))
+			{
+				PowerOf10 *= 10;
+				continue;
+			}
+			else if (!(PowerOf10 == 1))
+			{
+				if (State0)
+					State0 = 0;
+				PowerOf10 /= 10;
+				SolveCharacter = SolveNewNumber / PowerOf10 + '0';
+				SolveNewNumber %=  PowerOf10;
+			}
+			else if (State1)
+				SolveCharacter = ',', State1 = 0;
+			else if (SolveCharacter == ',')
+				SolveCharacter = ' ';
+			else if (SolveCharacter == ' ')
+				Number++;
 
-                        if (!(Number == SolveConditionOfWhile))
-                                _putchar(SolveCharacter);
-                }
-
-        }
+			if (!(Number == SolveConditionOfWhile))
+				_putchar(SolveCharacter);
+		}
+	}
 }
 
 /**
@@ -127,114 +123,112 @@ void print_between_0_9(int Number)
 
 void print_Beyond_98(int Number)
 {
-        unsigned char State0, State1, SolveCharacter;
-        int SolveConditionOfWhile, SolveNewNumber;
-        unsigned long int PowerOf10;
+	unsigned char State0, State1, SolveCharacter;
+	int SolveConditionOfWhile, SolveNewNumber;
+	unsigned long int PowerOf10;
 
-        State0 = State1 = 1;
-        SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
-        PowerOf10 = 10;
+	State0 = State1 = 1;
+	SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
+	PowerOf10 = 10;
 
-        while (Number > SolveConditionOfWhile)
-        {
-                if ((State0) && (Number / PowerOf10))
-                {
-                        PowerOf10 *= 10;
-                        continue;
-                }
-                else if (!(PowerOf10 == 1))
-                {
-                        if (State0)
-                                State0 = 0;
-                        PowerOf10 /= 10;
-                        SolveCharacter = SolveNewNumber / PowerOf10 + '0';
-                        SolveNewNumber %=  PowerOf10;
-                }
-                else if (State1)
-                        SolveCharacter = ',', State1 = 0;
-                else if (SolveCharacter == ',')
-                        SolveCharacter = ' ';
-                else if (SolveCharacter == ' ')
-                        Number--;
+	while (Number > SolveConditionOfWhile)
+	{
+		if ((State0) && (Number / PowerOf10))
+		{
+			PowerOf10 *= 10;
+			continue;
+		}
+		else if (!(PowerOf10 == 1))
+		{
+			if (State0)
+				State0 = 0;
+			PowerOf10 /= 10;
+			SolveCharacter = SolveNewNumber / PowerOf10 + '0';
+			SolveNewNumber %=  PowerOf10;
+		}
+		else if (State1)
+			SolveCharacter = ',', State1 = 0;
+		else if (SolveCharacter == ',')
+			SolveCharacter = ' ';
+		else if (SolveCharacter == ' ')
+			Number--;
 
-                if (!(Number == SolveConditionOfWhile))
-                        _putchar(SolveCharacter);
-        }
+		if (!(Number == SolveConditionOfWhile))
+			_putchar(SolveCharacter);
+	}
 }
 
 /**
- * print_Less_10 - Function that prints numbers less than 10.
+ * print_Less_0 - Function that prints numbers less than 0.
  * @Number: Variable type int.
  */
 
-void print_Less_10(int Number)
+void print_Less_0(int Number)
 {
-        unsigned char State0, State1, State2, SolveCharacter;
-        int SolveConditionOfWhile, SolveNewNumber;
-        unsigned long int PowerOf10;
+	if (Number < -9)
+	{
+		unsigned char State0, State1, State2, SolveCharacter;
+		int SolveConditionOfWhile, SolveNewNumber;
+		unsigned long int PowerOf10;
 
-        Number *= -1;
-        State0 = State1 = State2 = 1;
-        SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
-        PowerOf10 = 10;
+		Number *= -1;
+		State1 = State2 = 1;
+		SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
+		PowerOf10 = 10;
 
-        while (Number > SolveConditionOfWhile)
-        {
-                if (State2)
-                        SolveCharacter = '-', State2 = 0;
-                else if ((State0) && (Number / PowerOf10))
-                {
-                        PowerOf10 *= 10;
-                        continue;
-                }
-                else if (!(PowerOf10 == 1))
-                {
-                        if (State0)
-                                State0 = 0;
-                        PowerOf10 /= 10;
-                        SolveCharacter = (SolveNewNumber / PowerOf10) + '0';
-                        SolveNewNumber %=  PowerOf10;
-                }
-                else if (State1)
-                        SolveCharacter = ',', State1 = 0;
-                else if (SolveCharacter == ',')
-                        SolveCharacter = ' ';
-                else if (SolveCharacter == ' ')
-                        Number--, State2 = 1;
+		while (Number > SolveConditionOfWhile)
+		{
+			if (State2)
+				SolveCharacter = '-', State2 = 0;
+			else if ((State0) && (Number / PowerOf10))
+			{
+				PowerOf10 *= 10;
+				continue;
+			}
+			else if (!(PowerOf10 == 1))
+			{
+				if (State0)
+					State0 = 0;
+				PowerOf10 /= 10;
+				SolveCharacter = (SolveNewNumber / PowerOf10) + '0';
+				SolveNewNumber %=  PowerOf10;
+			}
+			else if (State1)
+				SolveCharacter = ',', State1 = 0;
+			else if (SolveCharacter == ',')
+				SolveCharacter = ' ';
+			else if (SolveCharacter == ' ')
+				Number--, State2 = 1;
 
-                if (!(Number == SolveConditionOfWhile))
-                        _putchar(SolveCharacter);
-        }
-}
+			if (!(Number == SolveConditionOfWhile))
+				_putchar(SolveCharacter);
+		}
+	}
+	else
+	{
+		int SolveConditionOfWhile, SolveNewNumber;
+		unsigned char State0, State1, State2, SolveCharacter;
 
-/**
- * print_between_The_Negative_0_9 - print the numbers from -9 to -1.
- * @Number: Varible type int.
- */
+		Number *= -1;
+		SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
+		State0 = State1 = State2 = 1;
 
-void print_between_The_Negative_0_9(int Number)
-{
-        int SolveConditionOfWhile, SolveNewNumber;
-        unsigned char State0, State1, State2, SolveCharacter;
+		while (Number < SolveConditionOfWhile)
+		{
+			if (State2)
+				SolveCharacter = '-', State2 = 0;
+			else if (State0)
+				SolveCharacter = Number % 10 + '0', State0 = 0;
+			else if (State1)
+				SolveCharacter = ',', State1 = 0;
+			else if (SolveCharacter == ',')
+				SolveCharacter = ' ';
+			else if (SolveCharacter == ' ')
+				Number--;
 
-        Number *= -1;
-        SolveNewNumber = Number, SolveConditionOfWhile = SolveNewNumber - 1;
-        State0 = State1 = State2 = 1;
+			if (!(Number == SolveConditionOfWhile))
+				_putchar(SolveCharacter);
+		}
 
-        while (Number < SolveConditionOfWhile)
-        {
-                if (State2)
-                        SolveCharacter = '-', State2 = 0;
-                else if (State0)
-                        SolveCharacter = Number % 10 + '0', State0 = 0;
-                else if (State1)
-                        SolveCharacter = ',', State1 = 0;
-                else if (SolveCharacter == ',')
-                        SolveCharacter = ' ';
-                else if (SolveCharacter == ' ')
-                        Number--;
-
-                if (!(Number == SolveConditionOfWhile))
-                        _putchar(SolveCharacter);
-        }
+	}
 }
