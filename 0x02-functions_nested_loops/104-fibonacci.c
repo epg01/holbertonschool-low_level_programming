4a842bdaa0d3faea714a47040007b77e630129c5
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-17 04:08:08 +0000

    Vamos a ver si nos da la salida

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,10 +7,12 @@
  */
 int main(void)
 {
-	unsigned long Total_Result = 2, Number = 1;
+	unsigned long int Total_Result = 2, Number = 1;
 	int count;
 
-	for (count = 0; count < 98; count++)
+	printf("%d, ", 1);
+
+	for (count = 1; count < 98; count++)
 	{
 		printf("%lu", Total_Result);
 		if (count != 97)
