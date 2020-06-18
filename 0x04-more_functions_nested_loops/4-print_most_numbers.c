5d09f13bcb9c4c7f9e8cd0f12674336997982f61
epg01 <60383826+epg01@users.noreply.github.com>
2020-06-18 12:58:16 -0500

    Output corrected

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,14 @@ void print_most_numbers(void)
 {
 	unsigned char SolveCharacter;
 
-	SolveCharacter = '9';
+	SolveCharacter = '0';
 	while (!(SolveCharacter == '\n' + 1))
 	{
+		if (!(SolveCharacter == '2') && (SolveCharacter != '4'))
 		putchar(SolveCharacter);
-		if ((SolveCharacter > '0') && (SolveCharacter <= '9'))
+		if ((SolveCharacter >= '0') && (SolveCharacter < '9'))
 			SolveCharacter--;
-		else if (SolveCharacter == '0')
+		else if (SolveCharacter == '9')
 			SolveCharacter = '\n';
 		else if (SolveCharacter == '\n')
 			SolveCharacter++;
