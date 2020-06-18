4e6210ea45a9430fd176c8f67da4793fbcd58459
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-18 22:58:59 +0000

    Exercise resolved

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Function that draws a diagonal line on the terminal.
+ * @Number: Quantity that must be positive (includes zero) in order for the
+ *          function to perform the task.
+ */
+
+void print_diagonal(int Number)
+{
+	unsigned char SolveCharacter = 0, State0 = 0;
+	unsigned char Counter_Space = 0, Counter_Character_Diagonal = 1;
+
+	while (SolveCharacter != '\n' + 1)
+	{
+		if (Number <= 0)
+			SolveCharacter = '\n', State0 = 1;
+		else if ((Counter_Character_Diagonal == 1) && !(Counter_Space))
+			SolveCharacter = '\\', Counter_Space++;
+		else if (Counter_Space < Counter_Character_Diagonal)
+			SolveCharacter = '_', Counter_Space++;
+		else if (((Counter_Space == Counter_Character_Diagonal) &&
+			  !(SolveCharacter == '\\')))
+			SolveCharacter = '\\';
+		else if (SolveCharacter == '\\')
+		{
+			Counter_Character_Diagonal++, Counter_Space = 1;
+			SolveCharacter = '\n';
+		}
+
+		putchar(SolveCharacter);
+		if (Number <= 0)
+			break;
+		else if ((Counter_Character_Diagonal == Number + 1) ||
+			(State0 == 1))
+			SolveCharacter = '\n' + 1;
+	}
+}
