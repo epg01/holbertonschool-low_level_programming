beef110f25db5baef8d9af8236887284b09a69fb
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-19 03:21:12 +0000

    Exercise performed

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Function that prints a triangle, followed by a new line.
+ * @Size: Variable type int that indicates the number of lines to be printed.
+ */
+
+void print_triangle(int Size)
+{
+	unsigned char SolveCharacter = 0, Counter_Space = 0;
+	unsigned char Counter_Character_hashtag = 1, State = 0;
+
+	while (!State)
+	{
+		if (Size <= 0)
+			SolveCharacter = '\n';
+		else if (Counter_Space < (Size - Counter_Character_hashtag))
+			SolveCharacter = '_', Counter_Space++;
+		else if ((Counter_Space >= (Size - Counter_Character_hashtag))
+			 && (Counter_Space < Size))
+			SolveCharacter = '#', Counter_Space++;
+		else if ((Counter_Space == Size) && (SolveCharacter != '\n'))
+			SolveCharacter = '\n';
+		else if (SolveCharacter == '\n')
+		{
+			Counter_Space = 0, Counter_Character_hashtag++;
+			SolveCharacter = 0;
+		}
+
+		if (!(Counter_Character_hashtag == Size + 1) &&
+		    (SolveCharacter))
+			_putchar(SolveCharacter);
+		else if (Size <= 0 || (Counter_Character_hashtag == Size + 1))
+			State = 1;
+	}
+}
