342393eb11b0b530921c1bd123687528e655567d
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-21 17:51:03 -0500

    Functions organized with conditions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,44 @@
 #include "holberton.h"
+
+
+#define POWER_OF_10    10
+
+
 /**
  * print_number - prints an integer char by char
- * @n: The integer to print
- *
- * Return: void
+ * @Number: The integer to print
  */
 
-void print_number(int n)
+void print_number(int Number)
 {
-	int i, size;
-	unsigned int num, num_temp, dig, div;
+	unsigned long int  SolveCharacter, PowerOf10;
+	unsigned char State;
 
-	num = n;
-	size = 0;
-	div = 1;
+	State = PowerOf10 =  1, SolveCharacter = 0;
 
-	if (n < 0)
+	while (PowerOf10)
 	{
-		num = num * -1;
-		_putchar(45);
+		if (Number < 0)
+		{
+			SolveCharacter = '-';
+			Number *= (-1);
+		}
+		else if (((Number / PowerOf10) >= POWER_OF_10) && (State))
+		{
+			PowerOf10 *= POWER_OF_10;
+			continue;
+		}
+		else if (State)
+		{
+			State = 0;
+			continue;
+		}
+		else if (POWER_OF_10)
+		{
+			SolveCharacter = (Number / PowerOf10) % POWER_OF_10 + '0';
+			PowerOf10 /= POWER_OF_10;
+		}
+		if (SolveCharacter)
+			_putchar(SolveCharacter);
 	}
-
-	num_temp = num;
-	while (num_temp != 0)
-	{
-		size++;
-		num_temp /= 10;
-
-	}
-	for  (i = 1; i < size; i++)
-	{
-		div = div * 10;
-	}
-	for (i = size - 1; i >= 1; i--)
-	{
-		dig = num / div;
-		_putchar((dig % 10) + '0');
-		div /= 10;
-	}
-	_putchar((num % 10) + '0');
 }
