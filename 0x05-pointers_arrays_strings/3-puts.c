00fd395b03a35e6ba988936150ef518bef3a3d70
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-23 16:45:40 +0000

    Function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/*
+ * Function declarations.
+ */
+
+signed int String_Character_Length(char *String_Character);
+
+/**
+ * _puts - Function that prints a string, followed by a new line.
+ * @String_Character: Variable that stores a character string.
+ */
+
+void _puts(char *String_Character)
+{
+	signed int Counter_Character;
+	unsigned char SolveCharacter;
+
+	Counter_Character = String_Character_Length(String_Character);
+	while (Counter_Character-- >= 0)
+	{
+		if (Counter_Character >= 0)
+			SolveCharacter = *String_Character++;
+		else
+			SolveCharacter = '\n';
+		_putchar(SolveCharacter);
+	}
+}
+
+signed int String_Character_Length(char *String_Character)
+{
+	int Counter_Character = 0;
+
+	while (*String_Character++)
+		Counter_Character++;
+
+	return (Counter_Character);
+}
