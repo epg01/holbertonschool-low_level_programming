c0325f3dcfeaf5a6adb38b38eeea5234179eef5e
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-23 15:54:59 +0000

    First exercise finished and made the hoberton file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Function to change any number to 98
+ * @Number: Variable to which the modification will be made.
+ */
+
+void reset_to_98(int *Number)
+{
+	unsigned char Swap = 98;
+
+	*Number = Swap;
+}
