1f29e198dd210934e01ca129851ca6ba6107f2a2
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 20:01:17 +0000

    Function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Function that reverses the content of an array of integer.
+ * @Pointer_Start: Pointer to array type int.
+ * @Size_Array: Size Array.
+ */
+
+void reverse_array(int *Pointer_Start, int Size_Array)
+{
+	if (n)
+	{
+		int temp, *Pointer_Last;
+
+		Pointer_Last = Pointer_Start, Pointer_Last += (Size_Array - 1);
+
+		while (Pointer_Start < Pointer_Last)
+		{
+			temp = *Pointer_Last;
+			*Pointer_Last-- = *Pointer_Start;
+			*Pointer_Start++ = temp;
+		}
+	}
+}
