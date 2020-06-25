8f106057f11036d581613ce51f1e7529385ec056
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 23:57:44 +0000

    Ejercicio terminado

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,49 +1,26 @@
 #include "holberton.h"
+
 /**
- * print_number - print integer
- * Return: void
- * @n: number
+ * print_number - function that draws a straight line in the terminal.
+ * @n: The number to be checked
+ * Return: (void)
  */
 void print_number(int n)
 {
-	unsigned int size = n;
-
-	int digits = 0;
-
-	int i;
+	unsigned int a;
 
 	if (n < 0)
-		size = -n;
-	if (n == 0)
-		digits = 1;
-
-	while (size >= 1)
 	{
-		size = size / 10;
-		digits++;
+		a = -n;
+		_putchar('-');
 	}
-
-	for (i = 0; i < digits; i++)
+	else
 	{
-
-		int pow = 1;
-
-		int j;
-
-		int d;
-
-		for (j = 0; j < digits - i - 1; j++)
-		{
-			pow = pow * 10;
-		}
-
-		d = ((n / pow) % 10);
-		if (n < 0)
-		{
-			d = -d;
-			if (i == 0)
-				_putchar(45);
-		}
-		_putchar(48 + d);
+		a = n;
 	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
 }
