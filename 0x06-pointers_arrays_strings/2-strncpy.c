0331616a6051f119195efe6e69a17aac00f9e756
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 22:38:34 +0000

    The else if condition has been removed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	char *Pointer_To_the_Start = dest;
 	char *Pounter_To_the_n = dest + n;
 
-	while (dest < Pounter_To_the_n && n)
+	while ((dest < Pounter_To_the_n) && n)
 	{
 		if (*src && n)
 			*dest++ = *src++, n--;
-		else if (n)
+		else
 			*dest++ = '\0';
 
 	}
