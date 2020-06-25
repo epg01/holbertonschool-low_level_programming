70f263888c30f260efe77b8dac33ff41f2915490
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 22:53:48 +0000

    The while condition was run

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,16 @@ char *_strncat(char *dest, char *src, int n)
 	unsigned char State = 1;
 	char *Pointer_To_the_Start = dest;
 
-	if (*dest)
+	while ((n && *src) || *dest)
 	{
-		while (n && *src)
-		{
-			if (*dest && State)
-				dest++;
-			else if (!(*dest) && State)
-				State = 0;
-			else
-				*dest++ = *src++, n--;
-		}
-	*dest = '\0';
+		if (*dest && State)
+			dest++;
+		else if (!(*dest) && State)
+			State = 0;
+		else
+			*dest++ = *src++, n--;
 	}
+	*dest = '\0';
+
 	return (Pointer_To_the_Start);
 }
