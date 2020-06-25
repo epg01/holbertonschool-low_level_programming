5870f49631a664bf9b68914b05ec52f84bae701b
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 19:14:17 +0000

    Exercise solved with the starting conditions

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *Pointer_To_the_Start = dest;
+	char *Pounter_To_the_n = dest + n;
 
-	while (n && *src)
-		*dest++ = *src++, n--;
-	if (!*src)
-		*dest = '\0';
+	while (dest <= Pounter_To_the_n && n)
+	{
+		if (*src && n)
+			*dest++ = *src++, n--;
+		else if (n)
+			*dest++ = '\0';
+
+	}
 	return (Pointer_To_the_Start);
 }
