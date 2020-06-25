29b81423bcfeb896edca7c7c46b52764dfd2b1d3
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 00:49:07 +0000

    Ejercicio terminado

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,64 +1,21 @@
-#include "holberton.h"
 #include <stdio.h>
 
-/*
- * declaration
- *void Print_Number(int *Puntero_Number);
- */
-
 /**
- * print_array - Function that prints n elements of an array of integers,
- *               followed by a new lines
- * @Punter_Number: pointer pointing to numbers in an array.
- * @Size_Matriz: Matrix size.
+ * print_array - print array
+ * @a: array
+ * @n: number iteration
+ * Return: none
  */
-
-void print_array(int *Puntero_Number, int Size_Matriz)
+void print_array(int *a, int n)
 {
-	char SolveCharacter;
+	int count, length;
 
-	while (Size_Matriz-- >= -2)
+	length = n - 1;
+	for (count = 0; count < n; count++)
 	{
-		if (Size_Matriz >= 0)
-			printf("%d", *Puntero_Number++);
-		else if (Size_Matriz == -1)
-			SolveCharacter = ('\n'), Size_Matriz--;
-		else if (Size_Matriz == -2)
-			SolveCharacter++, Size_Matriz--;
-		if (SolveCharacter == '\n')
-			putchar(SolveCharacter), Size_Matriz--;
-
-		if (Size_Matriz > 0)
+		printf("%d", a[count]);
+		if (count != length)
 			printf(", ");
 	}
+	putchar('\n');
 }
-/*
- *void Print_Number(int *Puntero_Number)
- *{
- *	signed long int Power_Of_10 = 1;
- *	unsigned char SolveCharacter = 0;
- *
- *	while ()
- *	{
- *		if (*Punter_Number / 10 > 10 && State0)
- *			Power_Of_10 *= POWER_OF_10;
- *		else if (State0)
- *			State0 = 0;
- *		else
- *		{
- *			if (*Puntero_Number < 0 && !SolveCharacter)
- *				SolveCharacter = '-';
- *			else if (Power_Of_10 >= 1)
- *			{
- *				SolveCharacter = *Puntero_Number;
- *				SolveCharacte /= (Power_Of10 % POWER_OF_10);
- *				SoveCharacter += '0';
- *				Power_Of_10 /= POWER_OF_10;
- *			}
- *			_putchar(SolveCharacter);
- *		}
- *		_putchar(',');
- *		_putchar(' ');
- *	}
- *}
- */
