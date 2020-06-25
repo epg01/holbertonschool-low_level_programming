690ca147a005eb73e6d29907135eba2b04e897de
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 23:59:15 +0000

    Ejercicio terminado

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,26 +1,48 @@
 #include "holberton.h"
 
-/**
- * print_number - function that draws a straight line in the terminal.
- * @n: The number to be checked
- * Return: (void)
- */
-void print_number(int n)
-{
-	unsigned int a;
+#define POWER_OF_10    10
 
-	if (n < 0)
+/**
+ * print_number - prints an integer char by char
+ * @Number: The integer to print
+ */
+
+void print_number(int Number)
+{
+	int  SolveCharacter;
+	signed long int PowerOf10;
+	unsigned char State, State0;
+
+	State = PowerOf10 =  State0 = 1, SolveCharacter = 0;
+
+	while (PowerOf10)
 	{
-		a = -n;
-		_putchar('-');
+		if ((Number < 0) && State0)
+			SolveCharacter = '-', State0 = 0;
+		else if (((Number / PowerOf10) >= POWER_OF_10 ||
+			  (Number / PowerOf10 <= -10)) && (State))
+		{
+			PowerOf10 *= POWER_OF_10;
+			continue;
+		}
+		else if (State)
+		{
+			State = 0;
+			continue;
+		}
+		else if (POWER_OF_10)
+		{
+			if (Number >= 0)
+				SolveCharacter = (Number / PowerOf10) % POWER_OF_10 + '0';
+			else
+			{
+				SolveCharacter = Number / PowerOf10 % 10;
+				SolveCharacter *= (-1);
+				SolveCharacter += '0';
+			}
+			PowerOf10 /= POWER_OF_10;
+		}
+		if (SolveCharacter)
+			_putchar(SolveCharacter);
 	}
-	else
-	{
-		a = n;
-	}
-	if (a / 10)
-	{
-		print_number(a / 10);
-	}
-	_putchar((a % 10) + '0');
 }
