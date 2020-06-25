7ccdf036f2d40de378d07f139a4e7c612cb03dfb
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 22:59:04 +0000

    Modify one of the conditions of the first if statement

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,11 @@ char *cap_string(char *String_Character)
 		if (*String_Character == ' '  || *String_Character == ','  ||
 		    *String_Character == '.'  || *String_Character == '!'  ||
 		    *String_Character == '\t' || *String_Character == '\n' ||
-		    *String_Character == '\"' || *String_Character == '('  ||
+		    *String_Character == '"' || *String_Character == '('  ||
 		    *String_Character == ')'  || *String_Character == '{'  ||
 		    *String_Character == '}'  || *String_Character == '?'  ||
 		    *String_Character == ';')
 		{
-			if (*String_Character == '\t')
-				*String_Character = ' ';
 			if (*(String_Character + 1) >= 'a' && *(String_Character + 1) <= 'z')
 				*(String_Character + 1) += ('A' - 'a'), String_Character++;
 			else
