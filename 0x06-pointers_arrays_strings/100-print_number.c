69d8da6f389c53629dfc0cb66d2123885ada3eb5
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 23:48:07 +0000

    Ejercicio terminado

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,23 +1,49 @@
 #include "holberton.h"
-
 /**
- * print_number - print intenger number
- * @n: integer
- *
- * Return: none
+ * print_number - print integer
+ * Return: void
+ * @n: number
  */
 void print_number(int n)
 {
-	int a;
+	unsigned int size = n;
+
+	int digits = 0;
+
+	int i;
 
 	if (n < 0)
+		size = -n;
+	if (n == 0)
+		digits = 1;
+
+	while (size >= 1)
 	{
-		_putchar('-');
-		a = n * -1;
+		size = size / 10;
+		digits++;
 	}
-	else
-		a = n;
-	if (n / 10)
-		print_number(a / 10);
-	_putchar(a % 10 + '0');
+
+	for (i = 0; i < digits; i++)
+	{
+
+		int pow = 1;
+
+		int j;
+
+		int d;
+
+		for (j = 0; j < digits - i - 1; j++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
+		if (n < 0)
+		{
+			d = -d;
+			if (i == 0)
+				_putchar(45);
+		}
+		_putchar(48 + d);
+	}
 }
