05b185f681ff3acb8aceb02486d4b572e38e2bfe
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 20:56:09 +0000

    Function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Function that capitalizes all words of a string.
+ * @String_Character: Stores the character string that is modified.
+ * Return: We return the start of the string.
+ */
+
+char *cap_string(char *String_Character)
+{
+	char *Pointer_Start = String_Character;
+
+	while (*String_Character)
+		if (*String_Character == ' '  || *String_Character == ','  ||
+		    *String_Character == '.'  || *String_Character == '!'  ||
+		    *String_Character == '\t' || *String_Character == '\n' ||
+		    *String_Character == '\"' || *String_Character == '('  ||
+		    *String_Character == ')'  || *String_Character == '{'  ||
+		    *String_Character == '}')
+		{
+			if (*(String_Character + 1) >= 'a' && *(String_Character + 1) <= 'z')
+				*(String_Character + 1) += ('A' - 'a'), String_Character++;
+			else
+				String_Character++;
+		}
+		else
+			String_Character++;
+	return (Pointer_Start);
+}
