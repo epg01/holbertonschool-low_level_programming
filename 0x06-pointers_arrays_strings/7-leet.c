162bbdd774ae43cda7c44b5c9e79436a178a4a11
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 23:39:52 +0000

    Ejercicio terminado

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - Encodes a string into 1337.
+ * @s: the string that will be procesed.
+ * Return: a char pointer to the encoded string.
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+
+	char *base="aAeEoOtTlL";
+	char *coded="4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; base[j] != '\0'; j++)
+		{
+			if (s[i] == base[j])
+			{
+				s[i] = coded[j];
+			}
+		}
+	}
+
+	return (s);
+}
