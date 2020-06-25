411e0eaec0a1626600975abe53cfbe7f7756a337
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-25 20:58:34 +0000

    I am missing putting some conditions in the first if statement

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,8 @@ char *cap_string(char *String_Character)
 		    *String_Character == '\t' || *String_Character == '\n' ||
 		    *String_Character == '\"' || *String_Character == '('  ||
 		    *String_Character == ')'  || *String_Character == '{'  ||
-		    *String_Character == '}')
+		    *String_Character == '}'  || *String_Character == '?'  ||
+		    *String_Character == ';')
 		{
 			if (*(String_Character + 1) >= 'a' && *(String_Character + 1) <= 'z')
 				*(String_Character + 1) += ('A' - 'a'), String_Character++;
