df4f680973e407c043a8c631019b26cefc6adec6
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-30 13:06:52 +0000

    The file was modified for a more efficient code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,34 @@
 #include "holberton.h"
 
-/**
- * print_chessboard - prints a chessboard
- * @a: pointer to a bidimensional array
- *
- * Return: none
- */
-void print_chessboard(char (*a)[8])
-{
-	int count, c;
+#define SIZETABLE   8
 
-	for (c = 0; c < 8; c++)
-	{
-		for (count = 0; count < 8; count++)
-			_putchar(a[c][count]);
-		_putchar(10);
-	}
+/**
+ * print_chessboard - Function that prints the chessboard.
+ * @
+ */
+
+void print_chessboard(char (*Pointer_Between_1_N_Array)[8])
+{
+	unsigned char SolveCharacter, State1, State2, State3;
+	signed   char Number_Of_Column;
+	unsigned char Number_Of_Line;
+
+	State1 = State2 = State3 = 1;
+	Number_Of_Column = Number_Of_Line = SIZETABLE;
+	SolveCharacter = 0;
+
+	while (Number_Of_Line || SolveCharacter == '\n')
+		if ((Number_Of_Column >= 0) && (State1))
+		{
+			SolveCharacter = (*Pointer_Between_1_N_Array)
+				[SIZETABLE - Number_Of_Column--];
+			State1 = 0;
+		}
+		else if ((Number_Of_Column == -1) && State2)
+		{
+			Number_Of_Column = SIZETABLE, Number_Of_Line--;
+			SolveCharacter = '\n', State2 = 0, Pointer_Between_1_N_Array++;
+		}
+		else
+			_putchar(SolveCharacter), State2 = State1 = 1, SolveCharacter = 0;
 }
