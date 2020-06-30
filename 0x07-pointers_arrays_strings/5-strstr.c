0e401b3605e75569e7e16bccfc5a326033792dd5
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-06-30 01:29:49 +0000

    The body of the function was organized

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,9 +3,11 @@
 #define NULL 0
 
 /**
- *
- *
- *
+ * _strstr - function that locates a substring.
+ * @haystack: function that locates a substring.
+ * @needle: Contains the array of characters (pattern).
+ * Return: Returns a pointer to the beginning of the located substring, or
+ *         NULL if the substring is not found.
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -13,12 +15,17 @@ char *_strstr(char *haystack, char *needle)
 	signed int Counter = 0;
 	char *temp = needle;
 
-	while (*haystack)
-		if (!(*needle))
-			break;
-		else if (*needle != *haystack)
-			needle = temp, haystack++;
-		else if (*needle++ == *haystack++)
-			Counter--;
-	return (!(*needle) ? haystack + Counter : NULL);
+	if (*haystack && *needle)
+	{
+		while (*haystack)
+			if (!(*needle))
+				break;
+			else if (*needle != *haystack)
+				needle = temp, haystack++;
+			else if (*needle++ == *haystack++)
+				Counter--;
+		return (!(*needle) ? haystack + Counter : NULL);
+	}
+	else
+		return (NULL);
 }
