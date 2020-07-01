335f8289f0f86b2e8f62e724b7fd71e468021eb6
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-01 15:54:37 +0000

    Solution of possible error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,18 @@
 
 /**
  * *_strchr - Function that locates a character in a string.
- * @s: Pointer to an array of characters.
- * @c: Pattern data to look for in the variable Array Character.
+ * @ArrayCharacter: Pointer to an array of characters.
+ * @Character: Pattern data to look for in the variable Array Character.
  * Return: Returns a pointer to the first occurrence of the character
  *         c in the string s, or NULL if the character is not found
  */
 
-char *_strchr(char *s, char c)
+char *_strchr(char *ArrayCharacter, char Character)
 {
-	while (*s)
-		if (*s != c)
-			s++;
-		else if (*s == c)
+	while (*ArrayCharacter)
+		if (*ArrayCharacter != Character)
+			ArrayCharacter++;
+		else if (*ArrayCharacter == Character)
 			break;
-	return ((*s) ? s : NULL);
+	return ((*ArrayCharacter || !(Character)) ? ArrayCharacter : NULL);
 }
