548be69f98d7c7365e673aca2eb799adc76c8bb2
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-02 02:19:54 +0000

    Ejercicio terminado

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,20 +1,47 @@
-#include "holberton.h"
 /**
- * wildcmp - compares two string
- * @s1: string1
- * @s2: string2
- * Return: 1 if equal 0 otherwise
+ * recursive_check - checks for wild cards recursively
+ *
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
+ *
+ * Return: 1 if identical, 0 for not, -1 for error
+ */
+int recursive_check(char *s1, char *s2, int wildC)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s2 == '*')
+		return (recursive_check(s1, s2 + 1, ++wildC));
+	else if (*s1 != *s2 && wildC > 0)
+	{
+		if (!*s1)
+			return (0);
+		return (recursive_check(s1 + 1, s2, wildC));
+	}
+	else if (*s1 == *s2 && wildC == 0)
+		return (recursive_check(s1 + 1, s2 + 1, 0));
+	else if (*s1 == *s2 && wildC > 0)
+	{
+		if (!recursive_check(s1 + 1, s2 + 1, 0))
+			return (recursive_check(s1 + 1, s2, wildC));
+		return (recursive_check(s1 + 1, s2 + 1, wildC));
+	}
+	else if (*s1 != *s2 && wildC == 0)
+		return (0);
+	return (-1);
+}
+/**
+ * wildcmp - compares two strings, supports wildcard '*'
+ *
+ * @s1: string 1 to compare to s2
+ * @s2: string 2 to compare to s1
+ *
+ * Return: 1 if identical, 0 otherwise, -1 for error
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 != *s2)
-		return (0);
-	else if (*s1 == 0 && *s2 == 0)
-		return (1);
-	else if (*s2 == '*')
-		wildcmp(s1 + 1, s2);
-	else if (*s2 == '*' && *s1 == *(s2 + 1))
-		return (wildcmp(s1 +1, s2 + 1));
-	return (wildcmp(s1 + 1, s2 + 1));
-
+	return (recursive_check(s1, s2, 0));
 }
