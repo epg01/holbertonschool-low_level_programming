91ffbfb4f62e8a2d9ba4fe45c2717a152ae813e5
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2020-07-08 04:56:32 +0000

    Ejercicio terminado

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,23 +1,69 @@
 #include "holberton.h"
 
-/**
- * **strtow - Pwdqwdwq
- * @str: The array
- *
- * Return: char
- */
+int count_words(char *s)
+{
+	int i, count;
 
+	for (i = 1; s[i]; i++)
+        {
+                if (s[i] != ' ' && s[i + 1] == ' ')
+                        count++;
+        }
+	return (count);
+}
+
+int count_chars(char *c)
+{
+	int i, count;
+
+	for (i = 0; c[i]; i++)
+	{
+		if (c[i] == ' ')
+			break;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * strtow - split a string into words
+ * @str: pointer to a tring
+ *
+ * Return: pointer to an array of strings.
+ */
 char **strtow(char *str)
 {
-	int i;
-	char **s;
+	int i, j, w = 0, words, chars;
+	char **p;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
-		;
-	s = malloc(i);
-	if (s == NULL)
-		return (NULL);
-	return (s);
+
+	words = count_words(str);
+
+	p = malloc(sizeof(char *) * words + 1);
+
+
+
+	for (i = 0; i < words ; i++)
+	{
+		for (j = w; str[j]; j++)
+		{
+			if (str[j] == ' ')
+				continue;
+			if (str[j] != ' ')
+			{
+				chars = count_chars(str + j);
+				printf("%i", chars);
+				w = j + chars;
+			}
+		}
+		p[i] = malloc(sizeof(char) * chars);
+		if (p[i] == NULL)
+		{
+			free(p[i]);
+			return (NULL);
+		}
+	}
+	return (p);
 }
