6cd27406c4766d6d12c9b3012a2ec595502e4036
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2020-07-08 02:59:25 +0000

    organized with the demands of betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,9 +4,12 @@ unsigned int Creando_Array(int **DoblePointer, int wigth, int height);
 void Inicializacion_Del_Array(int **Doble_Pointer, int width, int height);
 
 /**
- *
- *
- *
+ * alloc_grid - Function that returns a pointer to a 2 dimensional
+ *              array of integers
+ * @width:  Width array.
+ * @height: Height array.
+ * Return: The function should return NULL on failure
+ *         If width or height is 0 or negative, return NULL
  */
 
 int **alloc_grid(int width, int height)
@@ -28,19 +31,26 @@ int **alloc_grid(int width, int height)
 			}
 			else
 			{
-				free (Doble_Pointer);
+				free(Doble_Pointer);
 				return (NULL);
 			}
 		}
 		else
 		{
-			free (Doble_Pointer);
+			free(Doble_Pointer);
 			return (NULL);
 		}
 	}
 	else
 		return (NULL);
 }
+
+/**
+ * Inicializacion_Del_Array - Function to initialize the array with zeros.
+ * @Doble_Pointer: Pointer pointing to a group n of pointers.
+ * @width:  Width array.
+ * @height: Height array.
+ */
 
 void Inicializacion_Del_Array(int **Doble_Pointer, int width, int height)
 {
@@ -60,20 +70,22 @@ void Inicializacion_Del_Array(int **Doble_Pointer, int width, int height)
 }
 
 /**
- *
- *
- *
+ * Creando_Array - Function to create the matrix of width * height.
+ * @DoblePointer: Pointer pointing to a group n of pointers.
+ * @width:  Width array.
+ * @height: Height array.
+ * Return: Returns 1 if the zero array could not be created
+ *         where it was created.
  */
 
 unsigned int Creando_Array(int **DoblePointer, int width, int height)
 {
-        int Counter = height;
-        int *Temp = *DoblePointer;
-        unsigned char State1, State0;
+	int Counter = height;
+	unsigned char State1, State0;
 
-        State0 = State1 = 1;
-        while (State0)
-        {
+	State0 = State1 = 1;
+	while (State0)
+	{
 		if (Counter >= 0 && State1)
 		{
 			Counter--;
@@ -84,18 +96,15 @@ unsigned int Creando_Array(int **DoblePointer, int width, int height)
 			DoblePointer++;
 		else if (Counter < height)
 		{
-			Temp = *DoblePointer--;
+			free(*DoblePointer--);
 			Counter++;
-			State1--;
+			State1 = 0;
 		}
-
-		if (!Temp)
-			free(Temp);
 
 		if (Counter == height)
 			return (1);
 		else if (Counter == 0)
 			State0--;
-        }
+	}
 	return (0);
 }
