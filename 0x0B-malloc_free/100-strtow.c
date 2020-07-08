8afcfad0c9914d2ff92c15ea7daf2d1a7333216f
Emmanuel Palacio <emmanuelpalacio1@hotmail.com>
2020-07-08 04:57:40 +0000

    Ejercicio terminado

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,69 +1,100 @@
 #include "holberton.h"
-
-int count_words(char *s)
-{
-	int i, count;
-
-	for (i = 1; s[i]; i++)
-        {
-                if (s[i] != ' ' && s[i + 1] == ' ')
-                        count++;
-        }
-	return (count);
-}
-
-int count_chars(char *c)
-{
-	int i, count;
-
-	for (i = 0; c[i]; i++)
-	{
-		if (c[i] == ' ')
-			break;
-		count++;
-	}
-	return (count);
-}
-
 /**
- * strtow - split a string into words
- * @str: pointer to a tring
- *
- * Return: pointer to an array of strings.
+ * strtow - splits string into words
+ * @str: string
+ * Return: pointer to array of strings
  */
 char **strtow(char *str)
 {
-	int i, j, w = 0, words, chars;
-	char **p;
+	char **s;
 
-	if (str == NULL)
+	int words, i = 0, filler = 0;
+
+	if (str == NULL || *str == 0)
 		return (NULL);
-
-	words = count_words(str);
-
-	p = malloc(sizeof(char *) * words + 1);
-
-
-
-	for (i = 0; i < words ; i++)
+	words = getWords(str);
+	if (words == 0)
+		return (NULL);
+	s = malloc(sizeof(char *) * (words + 1));
+	if (s == NULL)
+		return (NULL);
+	while (str[i])
 	{
-		for (j = w; str[j]; j++)
+		if (str[i] != ' ')
 		{
-			if (str[j] == ' ')
-				continue;
-			if (str[j] != ' ')
+			int size = get_size_of_first(str + i);
+
+			s[filler] = malloc(sizeof(char) * (size + 1));
+			if (s[filler] == NULL)
 			{
-				chars = count_chars(str + j);
-				printf("%i", chars);
-				w = j + chars;
+				int k;
+
+				for (k = 0; k < filler; k++)
+				{
+					free(s[k]);
+				}
+				free(s);
+				return (NULL);
 			}
+			fill(s[filler], str + i, size);
+			filler++;
+			i += size - 1;
 		}
-		p[i] = malloc(sizeof(char) * chars);
-		if (p[i] == NULL)
-		{
-			free(p[i]);
-			return (NULL);
-		}
+		i++;
 	}
-	return (p);
+	s[words] = NULL;
+	return (s);
+}
+/**
+ * getWords - get number of words in string
+ * @s: string
+ * Return: words
+ */
+int getWords(char *s)
+{
+	int i = 0, words = 0;
+
+	while (s[i])
+	{
+		if (i == 0 && s[i] != ' ')
+			words++;
+
+		if (s[i] == ' ' && s[i + 1] && s[i + 1] != ' ')
+		{
+			words++;
+		}
+		i++;
+	}
+	return (words);
+}
+/**
+ * get_size_of_first - size of first word in string
+ * @s: string
+ * Return: size of word
+ */
+int get_size_of_first(char *s)
+{
+	int i = 0;
+
+	while (s[i] && s[i] != ' ')
+		i++;
+	return (i);
+}
+/**
+ * fill - fills s2 in s1
+ * @s1: string1
+ * @s2: string2
+ * @size: size of s2
+ * Return: void
+ */
+void fill(char *s1, char *s2, int size)
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[size] = '\0';
 }
