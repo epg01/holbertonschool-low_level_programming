e2271cdc7982a6549d3aaefac3597b2c128a1041
Emmanuel <emmanuelpalacio1@gmail.com>
2020-07-24 03:02:31 +0000

    Ejerciico echo

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include "lists.h"
+
 /**
- * before_main - prints a message
- *
+ * mystartupfun - prototype construct is the one
+ * that makes the execution of the function first
  * Return: void
  */
-void before_main(void)
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+ * myStartupFun - implementation of myStartupFun
+ * Return: void
+ */
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
