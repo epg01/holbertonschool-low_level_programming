bbacf244fa2173c17b211cd085387f4fcd90cdff
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-29 04:40:06 +0000

    Ejerciio tmerinadi

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,44 +1,57 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
 
 /**
- * print_listint_safe - Prints all the elements of a listint_t list.
- * @head: Head pointer
- * Return: number of elements.
+ * find_listint_loop_pl - finds a loop in a linked list
+ *
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts/returns, NULL if no loop
+ */
+listint_t *find_listint_loop_pl(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+/**
+ * print_listint_safe - prints a linked list, even if it
+ * has a loop
+ *
+ * @head: head of list to print
+ *
+ * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t size = 0;
-	size_t i;
-	const listint_t *array[500];
+	size_t len = 0;
+	int loop;
+	listint_t *loopnode;
 
-	if (head == NULL)
-		return (0);
+	loopnode = find_listint_loop_pl((listint_t *) head);
 
-/*node = head;*/
-
-	while (head)
+	for (len = 0, loop = 1; (head != loopnode || loop) && head != NULL; len++)
 	{
-		i = 0;
-		while (i < size)
-		{
-/*printf("addres [%lu] =[%p]\n", i, (void *)array[i]);*/
-			if (array[i] == head)
-			{
-				printf("-> [%p] %i\n", (void *)head, head->n);
-				return (size);
-			}
-			i++;
-		}
-
-		printf("[%p] %i\n", (void *)head, head->n);
-		array[size] = head;
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == loopnode)
+			loop = 0;
 		head = head->next;
-		size++;
-
 	}
 
-	return (size);
+	if (loopnode != NULL)
+		printf("-> [%p] %d\n", (void *) head, head->n);
+	return (len);
 }
