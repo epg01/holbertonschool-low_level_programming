d991fc060bce982929b465a68e74323136dd6874
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-07-29 04:36:58 +0000

    Ejercicio trmenad

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
- * free_listint_safe - function that print a linked list content
- * @h: header pointer
- * Return: amount of nodes
+ * free_listint_safe - Free lists with a loop.
+ * @h: Head pointer
+ * Return: number of elements.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t n = 0;
-	listint_t *tmp;
+	size_t size = 0;
+	size_t i;
+	listint_t *next;
+	listint_t *array[500];
 
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
 		return (0);
 
-	if (*h == NULL)
+	while (*h)
 	{
-		return (0);
+		i = 0;
+		while (i < size)
+		{
+/*printf("addres [%lu] =[%p]\n", i, (void *)array[i]);*/
+			if (array[i] == *h)
+			{
+/*printf("-> [%p] %i\n", (void *)head, head->n);*/
+				*h = NULL;
+				h = NULL;
+				return (size);
+			}
+			i++;
+		}
+
+		array[size] = *h;
+
+		next = (*h)->next;
+		(*h)->next = NULL;
+		free(*h);
+		*h = next;
+		size++;
 	}
 
-	while (*h != NULL)
-	{
-		tmp = *h;
-		*h = tmp->next;
-		free(tmp);
-		n++;
-	}
+
 	*h = NULL;
-	return (n);
+	h = NULL;
+
+	return (size);
 }
