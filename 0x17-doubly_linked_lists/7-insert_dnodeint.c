d4c6d524a1536f87b1a6ea799c4ab346b9038728
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-05 21:18:31 +0000

    Exercise Finish

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,34 @@
 #include "lists.h"
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n)
 {
+	if (!idx || !(*Head)->next)
+	{
+		dlistint_t *New_Element = NULL;
 
+		New_Element = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+		New_Element->n    = n;
+
+		if (!(*Head)->next && idx)
+			return (NULL);
+		if (!(*Head)->next || (*Head)->next)
+		{
+			New_Element->next = (*Head)->next;
+			New_Element->prev = (*Head);
+		}
+		else if (!(*Head)->prev)
+		{
+			New_Element->next = (*Head);
+			New_Element->prev = (*Head)->prev;
+			(*Head)->prev     = New_Element;
+		}
+
+		if ((*Head)->next)
+			(*Head)->next->prev = New_Element;
+		(*Head) = New_Element;
+		return (New_Element);
+	}
+	else
+		return (insert_dnodeint_at_index(&(*Head)->next, --idx, n));
 }
