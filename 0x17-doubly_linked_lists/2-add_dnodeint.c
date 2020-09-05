ed078a7753be104bb43ee49db768d73a653b8127
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-05 19:37:30 +0000

    The exercise was done again, with recursion

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,20 +15,20 @@ dlistint_t *add_dnodeint(dlistint_t **Element, const int Number)
 		dlistint_t *New_Element = NULL;
 
 		New_Element = (dlistint_t *)malloc(sizeof(dlistint_t));
+
 		if (!New_Element)
 			return (NULL);
 
 		New_Element->n = Number;
-		New_Element->next = *Element;
+		New_Element->next = (*Element);
 
 		if (!(*Element))
-			New_Element->prev = *Element;
+			New_Element->prev = (*Element);
 		else
 			New_Element->prev = (*Element)->prev;
-
-		*Element = New_Element;
+		(*Element) = New_Element;
 		return (New_Element);
 	}
 	else
-		return (add_dnodeint(&(*Element)->next, Number));
+		return (add_dnodeint(&(*Element)->prev, Number));
 }
