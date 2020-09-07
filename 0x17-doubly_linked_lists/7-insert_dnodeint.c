004cf81a170b4561c3faaa029eb6972d1f728413
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-07 21:28:10 +0000

    Exercie Finish

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,24 +7,51 @@ R_insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n);
 dlistint_t *
 insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n);
 
-dlistint_t * Back_Go(dlistint_t *Element, unsigned int idx, int n)
+/**
+ * Back_Go - Function that positions the pointer to the head of the
+ *           structure
+ * @Element: Pointer to one of the nodes of the structure.
+ * @idx: Index in which a node will be inserted.
+ * @n: Number will be inserted.
+ * Return: A pointer to a node in the structure
+ */
+
+dlistint_t *Back_Go(dlistint_t *Element, unsigned int idx, int n)
 {
 	if (!Element->prev)
 		return (R_insert_dnodeint_at_index(&(Element), idx, n));
 	else
 		return (Back_Go(Element->prev, idx, n));
 }
-dlistint_t * Forward(dlistint_t *Element, unsigned int idx, int n)
+
+/**
+ * Forward - Function that positions the pointer to the equivalent to
+ *           the index is stored in idx
+ * @Element: Pointer to one of the nodes of the structure.
+ * @idx: Index in which a node will be inserted.
+ * @n: Number will be inserted.
+ * Return: A pointer to a node in the structure
+ */
+
+dlistint_t *Forward(dlistint_t *Element, unsigned int idx, int n)
 {
 	if (!idx || !Element->next)
 	{
-		if (!Element->next)
+		if (!Element->next && idx)
 			idx = STATE;
 		return (R_insert_dnodeint_at_index(&(Element), idx, n));
 	}
 	else
 		return (Forward(Element->next, --idx, n));
 }
+
+/**
+ * R_insert_dnodeint_at_index - Insert a node recursively.
+ * @Head: Pointer to one of the nodes of the structure.
+ * @idx: Index in which a node will be inserted.
+ * @n: Number will be inserted.
+ * Return: A pointer to a node in the structure
+ */
 
 dlistint_t *
 R_insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n)
@@ -38,48 +65,70 @@ R_insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n)
 	else if (!idx || idx == STATE)
 	{
 		dlistint_t *New_Element;
-		New_Element = (dlistint_t*)malloc(sizeof(dlistint_t));
+
+		New_Element = (dlistint_t *)malloc(sizeof(dlistint_t));
 		New_Element->n = n;
 		if (!*Head)
 		{
 			New_Element->next = (*Head);
-			New_Element->prev = (*Head);
-			(*Head)           = New_Element;
+			New_Element->prev = (*Head), (*Head) = New_Element;
 			return (New_Element);
 		}
 		else if (!(*Head)->prev)
 		{
 			New_Element->next = (*Head);
 			New_Element->prev = (*Head)->prev;
-			(*Head)->prev     = New_Element;
-			(*Head)           = New_Element;
+			(*Head)->prev     = New_Element, (*Head) = New_Element;
 			return (New_Element);
 		}
 		else if (idx == STATE)
 		{
 			New_Element->next = (*Head)->next;
 			New_Element->prev = (*Head);
-			(*Head)->next     = New_Element;
+			(*Head)->next = New_Element;
 		}
 		else
 		{
 			New_Element->next   = (*Head);
 			New_Element->prev   = (*Head)->prev;
 			(*Head)->prev->next = New_Element;
-			(*Head)->prev       = New_Element;
+			(*Head)->prev = New_Element;
 		}
 		return (New_Element);
 	}
 	return ((*Head));
 }
 
+/**
+ * listint_len - function that Counter all the elements of a
+ *                dlistint_t list.
+ * @Element: pointer to a structure.
+ * @Counter: Number of nodes.
+ * Return: Return: the number of nodes.
+ */
+
+size_t listint_len(dlistint_t *Element, size_t Counter)
+{
+	if (!Element)
+		return (Counter);
+	else
+		return (listint_len(Element->next, ++Counter));
+}
+
+/**
+ * insert_dnodeint_at_index - Insert a node recursively.
+ * @Head: Pointer to one of the nodes of the structure.
+ * @idx: Index in which a node will be inserted.
+ * @n: Number will be inserted.
+ * Return: A pointer to a node in the structure
+ */
 
 dlistint_t *
 insert_dnodeint_at_index(dlistint_t **Head, unsigned int idx, int n)
 {
 	if (!Head)
 		return (NULL);
-	else if (idx <= dlistint_len(*Head))
+	else if (idx <= listint_len(*Head, 0))
 		return (R_insert_dnodeint_at_index(Head, idx, n));
 	else
 		return (NULL);
